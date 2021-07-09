dd055e6e59a56d9aa8f6162b5fb2823518ff221e
valeriapdm <60206024+valeriapdm@users.noreply.github.com>
2021-07-09 12:13:20 -0500

    Add files via upload

---
+++ todo lista con nuxt vuejs/lista (todolist)/backend/src/test/java/com/milanwittpohl/playgroundwebbackend/PlaygroundWebBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milanwittpohl.playgroundwebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundWebBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
